7f10664bf0f0e72b969e9d53ac9e6120541e4f75
Jason Lionel Wright <jason_lionel@hotmail.co.uk>
2018-11-27 13:30:51 +1100

    more stuff

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 
 #include <ncurses.h>
+#include <string.h>
 
 int main() {
 
@@ -7,8 +8,8 @@ int main() {
 	printw("This is a story about how!\n");
 	printw("I got better at programming\n");
 	printw("Because I refused to let my self-doubt define me anymore\n");
-	int i = 0;
-	printw("%d", i);
+	//int i = 0;
+	//printw("%d", i);
  
 	int a;
 	scanw("%d", &a);
@@ -16,9 +17,27 @@ int main() {
 	printw("%d", a);
 	//yay i got number input working i'm such a legend
 	
+	//ok so we're going to program a simple menu
+    printw("Enter 1 for beef, 2 for chicken or 3 for vegan\n");
+    scanw("%d", &a);
+    switch(a) {
+        case 1:
+            printw("you selected beef\n");
+            break;
+        case 2:
+            printw("you selected chicken\n");
+            break;
+        case 3:
+            printw("sorry we don't have a vegan option\n");
+            break;
+    } //ok cool it works
+
+    char list[10][20];
+    strcpy(list[0], "listtop");
+    printw("This is string: %s\n", list[0]);
+
 	
-	
-	getch();
+	getch(); //pls let me see what happened
 	endwin(); // Restore normal terminal behavior
 	return 0;
 }
