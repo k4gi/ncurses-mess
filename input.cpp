ab9fdf7cc92fb9e942bfc9993794e9590d4958b5
kagi <k4gi@pm.me>
2019-03-30 17:49:53 +1100

    why can i only get 135 characters?

--- input.cpp
+++ input.cpp
@@ -1,17 +1,17 @@
 #include <ncurses.h>
 #include <string>
 
-std::string getstring() {
+std::string getstring(WINDOW *display) {
 	std::string input;
 
 	nocbreak();
 	echo();
 
-	int ch = getch();
+	int ch = mvwgetch(display,1,0);
 
 	while( ch != '\n' ) {
 		input.push_back( ch );
-		ch = getch();
+		ch = wgetch(display);
 	}
 
 	cbreak();
@@ -22,12 +22,22 @@ std::string getstring() {
 
 int main() {
 	initscr();
+	cbreak();
+	noecho();
 
-	std::string get = getstring();
+	int y,x;
+	getmaxyx(stdscr,y,x);
 
-	mvprintw(10,0,get.c_str());
+	WINDOW *inwin = newwin(5,x,0,0);
+	wborder(inwin,' ',' ','=','=','#','#','#','#');
+	WINDOW *outwin = newwin(5,x,5,0);
+	box(outwin,0,0);
 
-	getch();
+	std::string get = getstring(inwin);
+
+	mvwprintw(outwin,1,1,"%d %s",get.length(),get.c_str());
+
+	wgetch(outwin);
 	endwin();
 	return 0;
 }
