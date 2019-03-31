b2ab2963017f8b92dae04a80c569c41256416d1f
kagi <k4gi@pm.me>
2019-03-31 15:06:46 +1100

    String input done, dummy output done, clearing the screen done, only plugboards now

--- enigma_front.cpp
+++ enigma_front.cpp
@@ -1,6 +1,45 @@
 #include <ncurses.h>
 #include <string>
 
+void clearWindow(WINDOW *win) {
+	int y,x;
+	getmaxyx(win,y,x);
+	for(int i=1;i<y-1;i++) {
+		for(int j=1;j<x-1;j++) {
+			mvwaddch(win,i,j,' ');
+		}
+	}
+}
+
+void clearLine(WINDOW *win, int liney) {
+	int y,x;
+	getmaxyx(win,y,x);
+	for(int i=1;i<x-1;i++) {
+		mvwaddch(win,liney,i,' ');
+	}
+}
+
+std::string getString(WINDOW *display) {
+	std::string input;
+
+	nocbreak();
+	echo();
+	curs_set(1);
+
+	int ch = mvwgetch(display,1,1);
+
+	while( ch != '\n' ) {
+		input.push_back( ch );
+		ch = wgetch(display);
+	}
+
+	cbreak();
+	noecho();
+	curs_set(0);
+
+	return input;
+}
+
 int main() {
 	initscr(); //init ncurses
 	cbreak(); //ctrl-c works
@@ -24,7 +63,7 @@ int main() {
 	mvwprintw(slotwin,0,3,"3:Slot settings");
 	mvwprintw(plugwin,0,3,"4:Plugboard settings");
 	mvwprintw(helpwin,0,3,"5:Information");
-	mvwprintw(helpwin,3,1,"Press 'q' to exit");
+	mvwprintw(helpwin,3,1,"Press '12345' to navigate, and 'q' to exit");
 	wrefresh(inwin);
 	wrefresh(outwin);
 	wrefresh(slotwin);
@@ -44,6 +83,7 @@ int main() {
 	int slot2 = 0;
 	int slot3 = 0;
 	std::string plug = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //is there a better way to store this one?
+	std::string input = ""; //the fabled secret message
 	//navigation variables
 	char in='q';
 	int sel=0;
@@ -53,20 +93,36 @@ int main() {
 		in = wgetch(helpwin);
 		switch(in) {
 		case '1':
+			clearWindow(inwin);
+			clearWindow(helpwin);
+			wrefresh(inwin);
 			mvwprintw(helpwin,1,1,"Selected: Input");
-			mvwprintw(helpwin,2,1,"Press Enter to finish typing (once that's implemented)");
+			mvwprintw(helpwin,2,1,"Press Enter to finish typing");
 			wrefresh(helpwin);
+			input = getString(inwin);
+			clearWindow(helpwin);
+			mvwprintw(helpwin,1,1,"Input done!");
 			break;
 		case '2':
+			clearWindow(outwin);
+			clearWindow(helpwin);
 			mvwprintw(helpwin,1,1,"Selected: Output");
-			mvwprintw(helpwin,2,1,"Press Enter to encode!(soon)");
+			mvwprintw(helpwin,2,1,"Press Enter to encode, any other key to exit");
 			wrefresh(helpwin);
+			if(wgetch(helpwin) == '\n') {
+				mvwprintw(outwin,1,1,input.c_str());
+			}
+			wrefresh(outwin);
+			clearWindow(helpwin);
+			mvwprintw(helpwin,1,1,"Output done!");
 			break;
 		case '3':
+			clearWindow(helpwin);
 			mvwprintw(helpwin,1,1,"Selected: Slot settings");
-			mvwprintw(helpwin,2,1,"Navigate submenu with 'hjkl', press 'd' when you're done");
+			mvwprintw(helpwin,3,1,"Navigate submenu with 'hjkl', press 'd' when you're done");
 			wrefresh(helpwin);
 			do {
+				clearWindow(slotwin);
 				for(int i=0;i<slotsize;i++) {
 					if(i==sel) wattron(slotwin,A_REVERSE); //attribute, reverse colours
 					mvwprintw(slotwin,i+1,1,slotoptions[i].c_str());
@@ -100,6 +156,7 @@ int main() {
 								break;
 							case 'h':
 								ref=sel2;
+								clearLine(helpwin,2);
 								mvwprintw(helpwin,2,1,"You have selected %s",refoptions[ref].c_str());
 								break;
 							}
@@ -124,14 +181,17 @@ int main() {
 								switch(sel) {
 								case 1:
 									slot1=sel2;
+									clearLine(helpwin,2);
 									mvwprintw(helpwin,2,1,"You have selected %s for Slot I",wheeloptions[slot1].c_str());
 									break;
 								case 2:
 									slot2=sel2;
+									clearLine(helpwin,2);
 									mvwprintw(helpwin,2,1,"You have selected %s for Slot II",wheeloptions[slot2].c_str());
 									break;
 								case 3:
 									slot3=sel2;
+									clearLine(helpwin,2);
 									mvwprintw(helpwin,2,1,"You have selected %s for Slot III",wheeloptions[slot3].c_str());
 									break;
 								}
@@ -144,22 +204,26 @@ int main() {
 				}
 			} while(in != 'd');
 			sel = 0;
-			mvwprintw(helpwin,1,1,"Done!");
-			wrefresh(helpwin);
+			clearWindow(slotwin);
+			wrefresh(slotwin);
+			clearWindow(helpwin);
+			mvwprintw(helpwin,1,1,"Slot settings done!");
 			break;
 		case '4':
+			clearWindow(helpwin);
 			mvwprintw(helpwin,1,1,"Selected: Plugboard settings");
 			mvwprintw(helpwin,2,1,"Press a letter, then another letter, and they get connected(soon)");
-			wrefresh(helpwin);
 			break;
 		case '5':
+			clearWindow(helpwin);
 			mvwprintw(helpwin,1,1,"Selected: Information");
 			mvwprintw(helpwin,2,1,"Yeah idk why you'd select this one maybe to 'q'uit the program?");
-			wrefresh(helpwin);
+			mvwprintw(helpwin,3,1,"Press '12345' to navigate, and 'q' to exit");
 			break;
 		default:
+			clearWindow(helpwin);
 			mvwprintw(helpwin,1,1,"You pressed the wrong key lol");
-			wrefresh(helpwin);
+			mvwprintw(helpwin,3,1,"Press '12345' to navigate, and 'q' to exit");
 		}
 	} while(in != 'q');
 
