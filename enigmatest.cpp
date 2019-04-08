f35d606db896271a3db80c287b3b938426b004df
kagi <k4gi@pm.me>
2019-04-08 11:44:37 +1000

    basic encoding test, looks good so far

--- enigmatest.cpp
+++ enigmatest.cpp
@@ -6,5 +6,14 @@
 int main() {
 	enigma_machine hello;
 	std::cout << "hello\n";
+	
+	hello.set_wheel(0,0,0);
+	hello.set_wheel(1,1,1);
+	hello.set_wheel(2,2,2);
+	hello.set_reflector(0);
+
+	std::string out = hello.encode("ppwhgcsc");
+	std::cout << out;
+
 	return 0;
 }
